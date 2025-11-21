7de9f4f9294177f96b0f0b6eeb5943a39e687ea8
Gabriel Xavier <k2.gabriel@tigre.com>
2025-11-21 11:14:32 -0300

    Removido teste fetch menu

--- server/api/layout/menu.get.ts
+++ server/api/layout/menu.get.ts
@@ -1,19 +1,5 @@
-import { albumService } from "@/../server/services/albums.service";
-import { postService } from "@/../server/services/posts.service";
-import { userService } from "@/../server/services/users.service";
-
 export default cachedEventHandler(
   async () => {
-    await albumService.getAll();
-    await postService.getAll();
-    await userService.getAll();
-
-    const [a, b, c] = await Promise.all([
-      albumService.getAll(),
-      postService.getAll(),
-      userService.getAll(),
-    ]);
-
     return [
       { label: "Home", url: "/" },
       { label: "Posts", url: "/posts" },
