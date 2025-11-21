c63971eac8542abba27f4d2e280a7f9cc0717324
Gabriel Xavier <k2.gabriel@tigre.com>
2025-11-21 10:50:09 -0300

    Novo commit

--- app/composables/useLayout.ts
+++ app/composables/useLayout.ts
@@ -1,18 +1,18 @@
-import type { MenuItem } from '@/../server/types/menu'
+import type { MenuItem } from "@/../server/types/menu";
 
 export function useLayout() {
-    const menu = useState<MenuItem[] | null>("layout-menu", () => [])
-    const footer = useState<MenuItem[] | null>("layout-footer", () => [])
+  const menu = useState<MenuItem[] | null>("layout-menu", () => []);
+  const footer = useState<MenuItem[] | null>("layout-footer", () => []);
 
-    const loadLayout = async () => {
-        const [responseMenu, responseFooter] = await Promise.all([
-            useFetch<MenuItem[]>('/api/layout/menu'),
-            useFetch<MenuItem[]>('/api/layout/footer')
-        ])
+  const loadLayout = async () => {
+    const [responseMenu, responseFooter] = await Promise.all([
+      useFetch<MenuItem[]>("/api/layout/menu"),
+      useFetch<MenuItem[]>("/api/layout/footer"),
+    ]);
 
-        menu.value = responseMenu?.data?.value ?? null
-        footer.value = responseFooter?.data?.value ?? null
-    }
+    menu.value = responseMenu?.data?.value ?? null;
+    footer.value = responseFooter?.data?.value ?? null;
+  };
 
-    return { menu, footer, loadLayout }
+  return { menu, footer, loadLayout };
 }
